e2539a9873157b6e3925652488cb7379f7a8e8c1
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-12 17:49:15 +0300

    active state done

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const subscribeBtn = document.getElementById('subscribe');
+const inputEmailEL = document.getElementById('email')
+
+
+subscribeBtn.addEventListener('click',validateEmail)
+
+const validateEmail = ()=>{
+
+    
+}
